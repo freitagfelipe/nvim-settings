b2563d1db664dcaec89190d8f24f70c79532f8d0
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2022-03-05 09:39:17 -0300

    Add a settings file for basic Nvim configurations

---
+++ lua/settings.lua
@@ -0,0 +1,16 @@
+local opt = vim.opt
+
+opt.wrap = false
+opt.encoding = "utf-8"
+opt.ruler = true
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.smartindent = true
+opt.number = true
+opt.numberwidth = 3
+opt.termguicolors = true
+opt.clipboard = "unnamedplus"
+opt.pumheight = 10
+opt.splitbelow = true
+opt.signcolumn = "yes"
