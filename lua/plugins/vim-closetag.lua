1571cd62d439257a4a36216a149a7b11c15dd219
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2022-03-11 20:17:53 -0300

    Add vim-closetag setup file

---
+++ lua/plugins/vim-closetag.lua
@@ -0,0 +1,8 @@
+local g = vim.g
+
+g.closetag_filetypes = "html, jsx, tsx"
+g.closetag_xhtml_filenames = "*.jsx, *.tsx"
+g.closetag_filenames = "*.html, *.jsx, *.tsx"
+g.closetag_emptyTags_caseSensitive = 0
+g.closetag_shortcut = '>'
+g.closetag_close_shortcut = ''
